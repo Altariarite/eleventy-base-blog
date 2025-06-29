e916fe84a845ab749057f98955fc1c187acd3e0e
altaria <yuanting.mao@gmail.com>
2025-06-29 13:50:16 +0100

    Update metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,10 @@
 export default {
-	title: "Eleventy Base Blog v9",
+	title: "Functional Wannabe",
 	url: "https://example.com/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
 		name: "Your Name Here",
-		email: "youremailaddress@example.com",
 		url: "https://example.com/about-me/"
 	}
 }
